b873ddd2a72c49795bd75cb5518c17f7521bb63d
Alekhova Elena <0allena0@gmail.com>
2013-01-15 23:08:27 +0400

    Added GenerateStripRowwise

--- generator.cpp
+++ generator.cpp
@@ -1,2 +1,21 @@
-#include "umvm.h"
+#include "umivm.h"
+#include <random>
 
+int GenerateStripRowwise(Ind StartRow, Ind EndRow, Ind StartColumn, Ind EndColumn, 
+                          int MinWeight, int MaxWeight, Matrix &m)
+/*Attention, columns indexes are not sorted by default*/
+/*The first version does not use MaxWeight parameter*/
+{
+    
+    m.clear();
+    std::default_random_engine generator(4);
+    std::uniform_int_distribution<unsigned int> distribution(0, EndColumn - StartColumn);
+    for (Ind i = StartRow; i < EndRow; i++)
+    {
+        for(int j = 0; j < MinWeight; j++)
+        {
+            m[StartRow + j].push_back( StartColumn + distribution(generator));
+        }     
+    }
+    return 0;
+}
